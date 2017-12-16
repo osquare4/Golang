9ced30fb50d603bb1c62ba22b6de63f58112d54c
Semiu <ab_semiu88@yahoo.co.uk>
2017-12-16 21:40:11 +0100

    Go Arithmetic

--- hello.go
+++ hello.go
@@ -2,5 +2,14 @@ package main
 import "fmt"
 
 func main(){
-   fmt.Println("Hello Go... I am coming")
+  firstNumber := 42.0
+  secondNumber := 89.0
+   fmt.Println("Addition = ", 
+firstNumber + secondNumber)
+   fmt.Println("Subtraction = ", 
+secondNumber - firstNumber)
+   fmt.Println("Multiplication = ", 
+firstNumber * secondNumber)
+   fmt.Println("Division = ", 
+secondNumber / firstNumber)
 }
